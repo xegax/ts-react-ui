f5167e906b77befacaf3aefa0aca37a377f4d40e
xega <xxegax@gmail.com>
2018-12-21 09:46:03 +0300

    typing for images

--- src/icon.tsx
+++ src/icon.tsx
@@ -3,12 +3,16 @@ import * as React from 'react';
 export const Icon = (props: { src: string, size?: string, width?: number; height?: number }): JSX.Element => {
   const width = props.width;
   const height = props.height;
+  let src = props.src;
+  if (src)
+    src = `url(${src})`;
+
   return (
     <div
       className={'icon-div ' + (props.size || '')}
       style={{
         width, height,
-        backgroundImage: `url(/data/image/${props.src})`
+        backgroundImage: src
       }}
     />
   );
